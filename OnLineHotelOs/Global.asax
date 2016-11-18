d7fd2fd98e60782218b638d23946406c87b8ef02
AiyeroKing <AiyeroKing@outlook.com>
2016-11-18 19:55:54 +0800

    添加项目文件。

---
+++ OnLineHotelOs/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OnLineHotelOs.MvcApplication" Language="C#" %>
---
+++ OnLineHotelOs/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnLineHotelOs.Startup))]
+namespace OnLineHotelOs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
