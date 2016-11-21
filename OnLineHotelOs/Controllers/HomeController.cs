7ac4a5145fed863e369f48d67d4984f8b4bea816
AiyeroKing <AiyeroKing@outlook.com>
2016-11-21 20:50:34 +0800

    主页步骤一

--- OnLineHotelOs/Controllers/HomeController.cs
+++ OnLineHotelOs/Controllers/HomeController.cs
@@ -8,17 +8,34 @@ namespace OnLineHotelOs.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index() /*酒店首页*/
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Location() /*酒店位置*/
         {
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Introduce() /*酒店介绍*/
+        {
+            return View();
+        }
+
+        public ActionResult Subscrube()/*住房预约*/
+        {
+            return View();
+        }
+        public ActionResult Check()/*住房查看*/
+        {
+            return View();
+        }
+        public ActionResult About()/*关于我们*/
+        {
+            return View();
+        }
+        public ActionResult Contact()/*联系方式*/
         {
             return View();
         }
